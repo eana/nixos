df333d866cdb18429723e2bdd5e8a4c276d75635
Jonas Eana <jonas@eana.ro>
2025-01-30 16:21:37 +0100

    Configure the terminal functionality in folke/snacks.nvim

--- assets/.config/nvim/lua/plugins/snacks-nvim.lua
+++ assets/.config/nvim/lua/plugins/snacks-nvim.lua
@@ -1,11 +1,27 @@
 return {
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    ------@type snacks.Config
-    ---opts = {
-    ---  dashboard = { enabled = false },
-    ---},
+  "folke/snacks.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    dashboard = { enabled = false },
+    terminal = {
+      win = {
+        relative = "editor",
+        position = "float",
+        width = 0.8,
+        height = 0.8,
+        border = "rounded",
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>\\",
+      function()
+        Snacks.terminal.toggle(nil, { cwd = vim.fn.getcwd() })
+      end,
+      mode = { "n", "t" },
+      desc = "Toggle Terminal",
+    },
   },
 }
