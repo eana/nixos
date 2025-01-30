6379dbb90abea9f2c62705ea41ad0c762beed375
Jonas Eana <jonas@eana.ro>
2025-01-30 16:20:57 +0100

    Add a short description for each disabled plugin

--- assets/.config/nvim/lua/plugins/disabled.lua
+++ assets/.config/nvim/lua/plugins/disabled.lua
@@ -1,15 +1,15 @@
 return {
-  { "echasnovski/mini.animate", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-  { "folke/noice.nvim", enabled = false },
-  -- { "lukas-reineke/indent-blankline.nvim", enabled = false },
-  { "nvimdev/dashboard-nvim", enabled = false },
-  { "rafamadriz/friendly-snippets", enabled = false },
-  -- { "zbirenbaum/copilot.lua", enabled = false },
+  { "echasnovski/mini.animate", enabled = false }, -- Provides animations for various Neovim actions
+  { "echasnovski/mini.ai", enabled = false }, -- Enhances text objects for better selection and manipulation
+  { "folke/flash.nvim", enabled = false }, -- Improves the search and jump functionality in Neovim
+  { "folke/noice.nvim", enabled = false }, -- Enhances the Neovim UI with better notifications and command line
+  -- { "lukas-reineke/indent-blankline.nvim", enabled = false }, -- Adds indentation guides to Neovim
+  { "rafamadriz/friendly-snippets", enabled = false }, -- Provides a collection of snippets for various languages
+  -- { "zbirenbaum/copilot.lua", enabled = false }, -- Integrates GitHub Copilot into Neovim
   -- themes
-  { "folke/tokyonight.nvim", enabled = false },
-  { "catppuccin/nvim", enabled = false },
-  -- { "nvim-lualine/lualine.nvim", enabled = false },
-  -- { "williamboman/mason-lspconfig.nvim", enabled = false },
-  -- { "williamboman/mason.nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false }, -- A clean, dark Neovim theme inspired by Tokyo Night
+  { "catppuccin/nvim", enabled = false }, -- A soothing pastel theme for Neovim
+  -- { "nvim-lualine/lualine.nvim", enabled = false }, -- A fast and easy-to-configure statusline for Neovim
+  -- { "williamboman/mason-lspconfig.nvim", enabled = false }, -- Bridges mason.nvim with lspconfig for easier LSP setup
+  -- { "williamboman/mason.nvim", enabled = false }, -- A package manager for Neovim that helps manage LSP servers, DAP servers, linters, and formatters
 }
