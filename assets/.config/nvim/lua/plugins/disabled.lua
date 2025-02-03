69af18afbace481670a41154c5ef7e07fcf3f5c5
Jonas Eana <jonas@eana.ro>
2025-02-03 17:43:30 +0100

    Disable additional neovim plugins and features

--- assets/.config/nvim/lua/plugins/disabled.lua
+++ assets/.config/nvim/lua/plugins/disabled.lua
@@ -3,6 +3,8 @@ return {
   { "echasnovski/mini.ai", enabled = false }, -- Enhances text objects for better selection and manipulation
   { "folke/flash.nvim", enabled = false }, -- Improves the search and jump functionality in Neovim
   { "folke/noice.nvim", enabled = false }, -- Enhances the Neovim UI with better notifications and command line
+  { "rcarriga/nvim-notify", enabled = false }, -- A fancy, configurable, notification manager for Neovim
+  { "folke/todo-comments.nvim", enabled = false }, -- Highlights and organizes TODO comments in your code
   -- { "lukas-reineke/indent-blankline.nvim", enabled = false }, -- Adds indentation guides to Neovim
   { "rafamadriz/friendly-snippets", enabled = false }, -- Provides a collection of snippets for various languages
   -- { "zbirenbaum/copilot.lua", enabled = false }, -- Integrates GitHub Copilot into Neovim
--- assets/.config/nvim/lua/plugins/snacks-nvim.lua
+++ assets/.config/nvim/lua/plugins/snacks-nvim.lua
@@ -3,7 +3,12 @@ return {
   lazy = false,
   priority = 1000,
   opts = {
+    animate = { enabled = false },
     dashboard = { enabled = false },
+    dim = { enabled = false },
+    explorer = { enabled = false },
+    scroll = { enabled = false },
+    zen = { enabled = false },
     terminal = {
       win = {
         relative = "editor",
